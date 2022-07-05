33be949e2ccdfc6e4e0edf8aefb5cb4dad0b0396
sairabbas <sair_abbas@yahoo.com>
2022-07-05 15:41:10 -0700

    css & side nav bar

--- src/App.js
+++ src/App.js
@@ -1,10 +1,9 @@
-import logo from './logo.svg';
 import './App.css';
 import SideNav from "./components/SideNav"
 
 export default function App() {
   return (
-      <div>
+      <div className="App">
         <SideNav />
       </div>
   )
--- src/components/SideNav.js
+++ src/components/SideNav.js
@@ -1,9 +1,23 @@
 import React from "react"
+import EmailRoundedIcon from "@material-ui/icons/EmailRounded";
+import LinkedInIcon from "@material-ui/icons/LinkedIn";
+import GitHubIcon from "@material-ui/icons/GitHub";
+import "../App.css"
 
 export default function SideNav(){
     return (
         <div>
-            <h1>SIDE NAVBAR</h1>
+            <div className="sidebar-logos" href="/" target="_blank">
+                <a href="mailto:sair_abbas@yahoo.com">
+                    <EmailRoundedIcon></EmailRoundedIcon>
+                </a>
+                <a href="https://github.com/sairabbas" target="_blank">
+                    <GitHubIcon></GitHubIcon>
+                </a>
+                <a href="https://www.linkedin.com/in/sairabbas" target="_blank">
+                    <LinkedInIcon></LinkedInIcon>
+                </a>
+            </div>
         </div>
     )
 }
